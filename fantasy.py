0fff66a218b1917356de9da193cd51737a310a2b
WP <warren.hyson5@gmail.com>
2022-06-28 18:47:39 -0400

    added python files

---
+++ fantasy.py
@@ -0,0 +1,5 @@
+from espn_api.football import League
+
+# Initialize fantasy football leageu
+league = League(league_id="1001903989", year=2021, username="hgoetz10@gmail.com", password="pitt7276")
+ 
